1c4fdf8c18e1b1f13d1bd06a5c2caf0cedf82925
vinny2001 <vincenzo.daria01@gmail.com>
2024-07-12 15:32:50 -0400

    Working node.js, rendering data to react is WIP

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,24 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
 import GameList from './components/GameList';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+          const response = await fetch(import.meta.env.VITE_GAMES_API_URL);
+            if (!response.ok) {
+                throw new Error('Data could not be fetched!');
+            }
+            console.log("Response: " + JSON.stringify(response));
+            const json_response = await response.json();
+            console.log("Json response: " + json_response);
+            setData(json_response); // assign JSON response to the data variable.
+        } catch (error) {
+            console.error('Error fetching games:', error);
+        }
+    };
+
+    fetchData();
+  }, []);
+
 
   return (
     <>
+    <Router>
       <div className="container-fluid">
 
         <div className="row" id="main-row">
           <h1 className="header text-center mt-3">Game List</h1>
         </div>
-        <GameList />
-
+        <Routes>
+          <Route path="/" element={<GameList data={data} />} />
+        </Routes>
       </div>
+    </Router>
+      
     </>
   )
 }
--- react/src/components/GameList.jsx
+++ react/src/components/GameList.jsx
@@ -1,25 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const GameList = () => {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    // Fetch games from the JSON file
-    fetch('./games.json')
-      .then(response => response.json())
-      .then(data => setGames(data))
-      .catch(error => console.error('Error fetching games:', error));
-  }, []);
-
+const GameList = (props) => {
+  
   return (
     <div className="container">
       <div className="row">
-        {games.map(game => (
-          <div key={game.id} className="col-md-4 col-sm-6 col-xs-12 mb-3">
+        {props.data.map(props => (
+          <div key={props.game.id} className="col-md-4 col-sm-6 col-xs-12 mb-3">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">{game.game_title}</h5>
+                <h5 className="card-title">{props.game.game_title}</h5>
               </div>
             </div>
           </div>
